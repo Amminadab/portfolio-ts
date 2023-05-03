1c6d56d97155df207c413afbe7630887f3744889
Amminadab <amminadabeliasfant@gmail.com>
2023-05-03 11:37:50 +0300

    added the languages using in work and languages that i use for fun

--- components/home/about/Stats.tsx
+++ components/home/about/Stats.tsx
@@ -16,16 +16,17 @@ export const Stats = () => {
             <span className="chip">TypeScript</span>
             <span className="chip">HTML</span>
             <span className="chip">CSS</span>
+            <span className="chip">Figma</span>
             <span className="chip">React</span>
             <span className="chip">Redux</span>
-            <span className="chip">NodeJS</span>
+            <span className="chip">Next</span>
+            <span className="chip">Vue</span>
+            <span className="chip">Tailwind</span>
+            <span className="chip">Linear</span>
             <span className="chip">Express</span>
-            <span className="chip">Postgres</span>
-            <span className="chip">MongoDB</span>
+            <span className="chip">Node</span>
             <span className="chip">GitHub</span>
-            <span className="chip">Jira</span>
-            <span className="chip">Heroku</span>
-            <span className="chip">AWS</span>
+            <span className="chip">Material UI</span>
           </div>
         </div>
       </Reveal>
@@ -37,15 +38,14 @@ export const Stats = () => {
           </h4>
           <div className={styles.statGrid}>
             <span className="chip">Rust</span>
-            <span className="chip">Tailwind</span>
             <span className="chip">Java</span>
             <span className="chip">Spring</span>
-            <span className="chip">Figma</span>
+            <span className="chip">Bootstrap</span>
             <span className="chip">Whimsical</span>
             <span className="chip">Planetscale</span>
             <span className="chip">GraphQL</span>
             <span className="chip">Python</span>
-            <span className="chip">FastAPI</span>
+            <span className="chip">RustAPI</span>
           </div>
         </div>
       </Reveal>
