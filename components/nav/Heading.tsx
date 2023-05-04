7093960ed5885b5709fcb9ea3cff825dffe4c2cf
Amminadab <amminadabeliasfant@gmail.com>
2023-05-04 13:36:29 +0300

    fixed the error whileopening resume

--- components/nav/Heading.tsx
+++ components/nav/Heading.tsx
@@ -6,7 +6,7 @@ export const Heading = () => {
   return (
     <header className={styles.heading}>
       <MyLinks />
-      <OutlineButton onClick={() => window.open("./Amminadab-CV-W-cert.pdf")}>
+      <OutlineButton onClick={() => window.open("/one-page-resume.pdf")}>
         My resume
       </OutlineButton>
     </header>
